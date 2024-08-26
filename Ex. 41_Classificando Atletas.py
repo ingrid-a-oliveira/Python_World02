0f53fc95d63bf7b826c3379e39bade08d0f658bf
Ingrid Oliveira <dev.ingridoliveira@gmail.com>
2024-08-26 20:52:12 -0300

    Atualização do código
    Atualização e complementação do código

--- Ex. 41_Classificando Atletas.py
+++ Ex. 41_Classificando Atletas.py
@@ -1,9 +1,22 @@
 #Estrutura
+from datetime import datetime
 print("Bem vindo à Confederação Nacional de Natação!\nVamos verificar em que categoria você se enquadra?")
 nome = input("Digite seu nome completo: ").title().split()
 primeiro_nome = nome[0]
 print(f"Seja muito bem vindo(a) {primeiro_nome}!")
-idade = int(input("Digite sua idade: "))
+
+#idade
+# idade = int(input("Digite sua idade: "))
+while True:
+    try:     
+        ano_nascimento = int(input("Digite o ano do seu nascimento: "))
+        if ano_nascimento > 1 and ano_nascimento < datetime.now().year:
+            idade = datetime.now().year - ano_nascimento
+            break
+        else: 
+            print(f"{primeiro_nome}, o ano de nascimento deve ser um número válido entre 1 e {datetime.now().year - 1}.")
+    except ValueError:
+        print(f"{primeiro_nome}, o ano de nascimento deve ser composto apenas por números inteiros. Tente novamente!")
 
 #Condições
 if idade <= 9:
@@ -14,7 +27,5 @@ elif idade <= 19:
     print(f"{primeiro_nome}, você faz parte da categoria ''JÚNIOR''! Verifique em nosso site os documentos necessários para sua inscrição!")
 elif idade <= 25:
     print(f"{primeiro_nome}, você faz parte da categoria ''SÊNIOR''! Verifique em nosso site os documentos necessários para sua inscrioão!")
-elif idade > 25:
-    print(f"{primeiro_nome}, você faz parte da categoria ''MASTER''! Verifique em nosso site os documentos necessários para sua inscrição!")
 else:
-    print(f"{primeiro_nome}, tivemos um problema! Verifique se a idade inserida consta apenas valores numéricos e tente novamente!")
+    print(f"{primeiro_nome}, você faz parte da categoria ''MASTER''! Verifique em nosso site os documentos necessários para sua inscrição!")
